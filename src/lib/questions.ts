fe1f7c30ea65201abcc38fb99c6ef340fac5bfee
thiotimoo <timoothio@gmail.com>
2024-03-12 13:28:57 +0700

    Test Path Join

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,11 +1,12 @@
 import { promises as fs } from "fs";
+import path from "path";
 let file: any;
 let data: any;
 
 export async function getAllQuestions() {
     if (!file)
         file = await fs.readFile(
-            process.cwd() + "/data/questions.json",
+            path.join(process.cwd(), "/data/questions.json"),
             "utf8"
         );
     if (!data && file) data = JSON.parse(file);
