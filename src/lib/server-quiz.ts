814ceb52a06a195786083e0aff9ba77c53aca7da
thiotimoo <timoothio@gmail.com>
2024-04-12 15:32:46 +0700

    New Result

--- src/lib/server-quiz.ts
+++ src/lib/server-quiz.ts
@@ -5,13 +5,32 @@ import { getAllQuestions } from "./questions";
 registerFont(path.join(process.cwd(), "/data/assets/fonts/Jua-Regular.ttf"), {
     family: "Jua",
 });
-
+export async function quizGenerateCharacter(points: number) {
+// 1. Bodoamatan (Tika): -22 hingga -11
+// 2. Waras (Wila) : -10 hingga 2
+// 3. Mikir (Pito) : 3 hingga 12
+// 4. Ovt (Rira): 13 hingga 22
+// 5. Sering ovt (Vina): 23 hingga 28
+    let character;
+    if (points <= 28 && points >= 23) {
+        character = "vina"
+    }else if(points <= 22 && points >= 13) {
+        character="rira"
+    }else if(points <= 12 && points >= 3) {
+        character="pito"
+    }else if(points <= 2 && points >= -10) {
+        character="wila"
+    }else if(points <= -11 && points >= -22) {
+        character="tika"
+    }
+    return `/data/assets/result_${character}.jpg`
+}
 export async function quizGenerateImage(result: any) {
     const canvas = createCanvas(1080, 1920);
     const ctx = canvas.getContext("2d");
 
     const image = await loadImage(
-        path.join(process.cwd(), "/data/assets/result-ovito.jpg")
+        path.join(process.cwd(), quizGenerateCharacter(result.points))
     );
     ctx.drawImage(image, 0, 0, 1080, 1920);
 
