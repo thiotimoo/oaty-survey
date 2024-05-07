e8b71e756e6623d26898c238be0d390e7d8f05e0
thiotimoo <timoothio@gmail.com>
2024-05-07 18:14:52 +0700

    Fixing Preload

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -8,14 +8,14 @@ const QuizPage = async ({ params }: any) => {
     return (
         <>
             <Head>
-                <link rel="preload" href="/assets/backgrounds/omori-transparent.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/female-transparent.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg1.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg2.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg3.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg4.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg5.webp" as="image" />
-                <link rel="preload" href="/assets/backgrounds/bg6.webp" as="image" />
+                <link rel="preload" href="/assets/omori-transparent.webp" as="image" />
+                <link rel="preload" href="/assets/female-transparent.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg1.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg2.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg3.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg4.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg5.webp" as="image" />
+                <link rel="preload" href="/backgrounds/bg6.webp" as="image" />
             </Head>
             <QuizLayout />
         </>
