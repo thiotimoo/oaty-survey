6c8f5107f31b61edb12fa8a0e5b4c833002e095f
thiotimoo <timoothio@gmail.com>
2024-05-07 18:11:33 +0700

    JPEG

--- src/lib/server-quiz.ts
+++ src/lib/server-quiz.ts
@@ -6,45 +6,52 @@ registerFont(path.join(process.cwd(), "/public/assets/fonts/Jua-Regular.ttf"), {
     family: "Jua",
 });
 export function quizGenerateCharacter(points: number) {
-// 1. Bodoamatan (Tika): -22 hingga -11
-// 2. Waras (Wila) : -10 hingga 2
-// 3. Mikir (Pito) : 3 hingga 12
-// 4. Ovt (Rira): 13 hingga 22
-// 5. Sering ovt (Vina): 23 hingga 28
+    // 1. Bodoamatan (Tika): -22 hingga -11
+    // 2. Waras (Wila) : -10 hingga 2
+    // 3. Mikir (Pito) : 3 hingga 12
+    // 4. Ovt (Rira): 13 hingga 22
+    // 5. Sering ovt (Vina): 23 hingga 28
     let character;
     if (points >= 26) {
-        character = "vina"
-    }else if(points <= 25 && points >= 11) {
-        character="rira"
-    }else if(points <= 10 && points >= 1) {
-        character="pito"
-    }else if(points <= 0 && points >= -16) {
-        character="wila"
-    }else{
-        character="tika"
+        character = "vina";
+    } else if (points <= 25 && points >= 11) {
+        character = "rira";
+    } else if (points <= 10 && points >= 1) {
+        character = "pito";
+    } else if (points <= 0 && points >= -16) {
+        character = "wila";
+    } else {
+        character = "tika";
     }
-    return `result-${character}.jpg`
+    return `result-${character}.jpg`;
 }
 export async function quizGenerateImage(result: any) {
-    const canvas = createCanvas(1080 / 4, 1920 / 4);
+    const canvas = createCanvas(1080, 1920);
     const ctx = canvas.getContext("2d");
 
     const image = await loadImage(
-        path.join(process.cwd(), 'public', 'results', quizGenerateCharacter(result.points).toString())
+        path.join(
+            process.cwd(),
+            "public",
+            "results",
+            quizGenerateCharacter(result.points).toString()
+        )
     );
-    ctx.drawImage(image, 0, 0, 1080 / 4, 1920 / 4);
+    ctx.drawImage(image, 0, 0, 1080/2, 1920/2);
+
+    ctx.font = "20px Jua";
+    ctx.textAlign = "left";
+    ctx.fillText(result.user.username, 100/2, 125/2);
 
     ctx.font = "10px Jua";
     ctx.textAlign = "left";
-    ctx.fillText(result.user.username, 100 / 4, 125 / 4);
-
-    ctx.font = "5px Jua";
-    ctx.textAlign = "left";
-    ctx.fillText(result.id, 100 / 4, 165 / 4);
-    return canvas.toDataURL();
+    ctx.fillText(result.id, 100/2, 165/2);
+    var quality = 0.7;
+    var dataUri = canvas.toDataURL("image/jpeg", quality);
+    return dataUri;
 }
 
-export async function quizCalculatePoints(answers: any){
+export async function quizCalculatePoints(answers: any) {
     const allQuestion = await getAllQuestions();
 
     let points = 0;
